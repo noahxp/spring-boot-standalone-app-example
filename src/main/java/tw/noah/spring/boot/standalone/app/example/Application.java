a042cbaa8d4239a4bfa02d10b05ff30180a3f986
Noah Liu <noahxp@gmail.com>
2018-07-06 20:26:32 +0800

    fix gradle syntax

--- src/main/java/tw/noah/spring/boot/standalone/app/example/Application.java
+++ src/main/java/tw/noah/spring/boot/standalone/app/example/Application.java
@@ -8,13 +8,9 @@ import org.springframework.context.ApplicationContext;
 import tw.noah.spring.boot.standalone.app.example.model.MailConfig;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(MailConfig.class)
 @Log4j2
-//@EnableConfigurationProperties(NumberModel.class)
 public class Application
 {
-
-//	private final static Logger log = LogManager.getLogger();
 
 	public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
